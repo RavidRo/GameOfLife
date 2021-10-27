78fe2b630c11b9fe74ded4f480792336ea36befe
Ravid Rom <ravidro@post.bgu.ac.il>
2021-10-27 20:13:11 +0300

    Implemented to pausing and adding/removing cells

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import gameoflife as gol
 import pygame, sys
 
 
-def drawCell(x, y, cell):
+def draw_cell(x, y, cell):
     x_pixel = x * settings.CELL_SIZE
     y_pixel = y * settings.CELL_SIZE
     pygame.draw.rect(
@@ -20,15 +20,28 @@ def drawCell(x, y, cell):
     )
 
 
-def drawBoard(board):
+def draw_board(board):
     for row in range(len(board)):
         for column in range(len(board[row])):
-            drawCell(column, row, board[row][column])
+            draw_cell(column, row, board[row][column])
+
+
+def screen_to_board_position(position):
+    x, y = position
+    x /= settings.CELL_SIZE
+    y /= settings.CELL_SIZE
+    return int(x), int(y)
 
 
 if __name__ == "__main__":
 
+    # Initial variables
     board = gol.initBoard()
+    paused = False
+    # Setting the GUI
+    pygame.init()
+    screen = pygame.display.set_mode(settings.SCREEN_SIZE)
+    pygame.display.set_caption("Game Of Life")
 
     # Glider
     board[0][1] = True
@@ -43,16 +56,25 @@ if __name__ == "__main__":
     # 4. Replace old board with new
     # 5. Repeat forever
 
-    pygame.init()
-
-    screen = pygame.display.set_mode(settings.SCREEN_SIZE)
-
     while True:
         for event in pygame.event.get():
+            # Exit safely
             if event.type == pygame.QUIT:
                 sys.exit()
+            # Get user input
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    paused = not paused
 
-        drawBoard(board)
-        board = gol.makeNextBoard(board)
-        pygame.time.delay(int(1 / settings.FRAME_RATE * 1000))
-        pygame.display.flip()
+            if any(pygame.mouse.get_pressed()):
+                mouse_pos = pygame.mouse.get_pos()
+                x, y = screen_to_board_position(mouse_pos)
+                board[y][x] = not board[y][x]
+                draw_cell(x, y, board[y][x])
+                pygame.display.flip()
+
+        if not paused:
+            draw_board(board)
+            board = gol.makeNextBoard(board)
+            pygame.time.delay(int(1 / settings.FRAME_RATE * 1000))
+            pygame.display.flip()
